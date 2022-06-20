5fd7944933f4f330acf86d782897ca0bd4b06f99
Stephen Nerby <stephen.a.nerby@gmail.com>
2022-06-20 00:57:10 -0400

    commit tree f94f4104a7b66998086d1ac169b33007a72dee3c

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App(){
+    const [puppo, setPuppo] = useState(null);
+
+    useEffect(() =>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                setPuppo(data.message)
+            });
+    }, []);
+        
+    if (!puppo) return <p>Loading...</p>;
+
+    return <img src={puppo} alt="A Random Dog"  />
+}
+
+export default App;
